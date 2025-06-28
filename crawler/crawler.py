d0d6afac457aae2305bc31b7f56f2afccd9682f8
kang360 <kkmhss702@naver.com>
2025-06-28 23:14:40 +0900

    🆕 최신 코드 반영: Crawler수정

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -4,7 +4,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from sqlalchemy import create_engine, text
 from dotenv import load_dotenv
 import pandas as pd
-from .sites import lukina, rise, ottogi, supernova, bigboss
+from crawler.sites import lukina, rise, ottogi, supernova, bigboss
 
 load_dotenv()
 
