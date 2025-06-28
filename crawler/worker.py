5e2897bab664c22ce6229ba8455d7b5004933f71
kang360 <kkmhss702@naver.com>
2025-06-28 23:50:48 +0900

    🆕 최신 코드 반영: Crawler수정

--- crawler/worker.py
+++ crawler/worker.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     print("📦 Render Worker: 크롤링 실행 시작")
     run_crawler()
     print("✅ 크롤링 완료")
-    time.sleep(600)
+    time.sleep(900)
     print("🔔 예약 상태 확인 및 이메일 발송 시작")
     check_reservation_alerts()
     print("✅ 예약 상태 확인 및 이메일 발송 완료")
